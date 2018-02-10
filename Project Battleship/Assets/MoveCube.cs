6c5c13dcb159bdd5f71313f1a8ab0326bf2952d0
Darkpony <Darkpony>
2018-02-09 16:59:30 -0900

    Added new scene

---
+++ Project Battleship/Assets/MoveCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+    public GameObject cubeprefab;
+    int width = 11;
+    int height = 11;
+
+    // Use this for initialization
+    void Start() {
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Instantiate(cubeprefab, new Vector3(x, 0, y), Quaternion.identity);
+            }
+        }
+    }
+}
+          
+
+        
+
+        
+		
+	
