465464f5e4a873cae28034c4bab6d1e1bc48a234
Darkpony <Darkpony>
2018-02-13 11:30:02 -0900

    Added scene switching

--- Project Battleship/Assets/Scripts/FireGrid.cs
+++ Project Battleship/Assets/Scripts/FireGrid.cs
@@ -1,20 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 
 public class FireGrid : MonoBehaviour {
 
    
     public int selGridInt = 0;
-    public string[] selStrings = new string[] { "A1", "A2", "A3" };
+    public string[] selStrings = new string[] { "A1", "B1", "C1", "D1", "E1", "F1", "A2", "B2", "C2", "D2", "E2",
+                                                "F2", "A3", "B3", "C3", "D3", "E3", "F3", "A4", "B4", "C4", "D4", "E4", "F4",
+                                                 "A5", "B5", "C5", "D5", "E5", "F5", "A6", "B6", "C6", "D6", "E6", "F6"};
+    public string[] selStrings1 = new string[] { "A1", "A2", "A3", "A4", "A5", "A6" };
 
     void OnGUI() {
-        GUILayout.BeginArea(new Rect(20, 240, 100, 100));
-        var kvpArr = new List<KeyValuePair<string[], Vector3>>();
-        selGridInt = GUILayout.SelectionGrid(selGridInt, selStrings, 1, GUILayout.Width(30));
+        GUILayout.BeginArea(new Rect(20, 100,1000,1000));
+        //   GUILayout.BeginVertical("Box");
+        // selGridInt = GUILayout.SelectionGrid(selGridInt, selStrings, 2, GUILayout.Width(20));
+        selGridInt = GUILayout.SelectionGrid(selGridInt, selStrings, 6, GUILayout.Width(200));
         if (GUILayout.Button("FIRE", GUILayout.Width(50)))
-            Debug.Log("You chose " + selStrings[selGridInt]);
+            // Debug.Log("You chose " + selStrings[selGridInt]);
+            SceneManager.LoadScene("fireScene");
         GUILayout.EndArea();
+       // GUILayout.EndVertical();
+     
     }
+
 }
--- Project Battleship/Assets/Scripts/WindowExample.cs
+++ Project Battleship/Assets/Scripts/WindowExample.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class WindowExample : MonoBehaviour {
+
+    public GameObject win = GameObject.Find("Window");
     public Rect windowRect = new Rect(40, 200, 120, 50);
     void OnGUI() {
         windowRect = GUI.Window(0, windowRect, DoMyWindow, "My Window");
-
+        DontDestroyOnLoad(win);
     }
     void DoMyWindow(int windowID) {
         if (GUI.Button(new Rect(10, 20, 100, 20), "Okay"))
